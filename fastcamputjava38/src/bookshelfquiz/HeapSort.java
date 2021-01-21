c012a7e6eaf42758785ac2ad4edada691785c766
jaeeunhyun <rosa2070@naver.com>
2021-01-22 01:22:20 +0900

    four

---
+++ fastcamputjava38/src/bookshelfquiz/HeapSort.java
@@ -0,0 +1,28 @@
+package bookshelfquiz;
+
+public class HeapSort implements Sort{
+
+	@Override
+	public void ascending(int[] arr) {
+		System.out.println("HeapSort ascending");
+	}
+
+	@Override
+	public void descending(int[] arr) {
+		System.out.println("HeapSort descending");
+		
+	}
+
+	@Override
+	public void description() {
+		Sort.super.description();
+		System.out.println("HeapSort ¿‘¥œ¥Ÿ");
+
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ fastcamputjava38/src/bookshelfquiz/QuickSort.java
@@ -0,0 +1,27 @@
+package bookshelfquiz;
+
+public class QuickSort implements Sort{
+
+	@Override
+	public void ascending(int[] arr) {
+		System.out.println("QuickSort ascending");
+	}
+
+	@Override
+	public void descending(int[] arr) {
+		System.out.println("QuickSort descending");
+		
+	}
+
+	@Override
+	public void description() {
+		Sort.super.description();
+		System.out.println("QuickSort ¿‘¥œ¥Ÿ");
+	}
+	
+	
+	
+	
+	
+
+}
