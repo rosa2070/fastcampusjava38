ed046be350c62ef57ec92d15a0309f909ca4a834
jaeeunhyun <rosa2070@naver.com>
2021-01-22 23:05:10 +0900

    three

---
+++ fastcamputjava38/src/generic/GenericPrinterTest.java
@@ -0,0 +1,11 @@
+package generic;
+
+public class GenericPrinterTest {
+
+	public static void main(String[] args) {
+		
+		GenericPrinter<Powder> powderPrinter = new GenericPrinter<Powder>();
+		System.out.println(powderPrinter);
+	}
+
+}
